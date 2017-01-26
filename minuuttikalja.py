cf461c0dca37c5e122d6de2ab9bab1550f0dadb2
AntsaE <antero.eravuori@gmail.com>
2017-01-26 13:31:51 +0100

    Redesigned input functions in input_screen.py and made a function for adding players in player.py

--- minuuttikalja.py
+++ minuuttikalja.py
@@ -4,14 +4,38 @@ from time import time
 from drawer import *
 from timer import Timer
 
-
+from input_screen import *
+from player import *
 
 
 def main():
+
     pg.init()      
     surface_height = 500   
     surface_width = 1000
     main_surface = pg.display.set_mode((surface_width, surface_height))
+    
+
+    players = []
+    more_players = True
+    while more_players:
+        ask = "Press Enter for new player, or anything else to start"
+        fontobject = pg.font.Font(None,46)
+        main_surface.blit(fontobject.render(ask, 1, (255,255,255)), (main_surface.get_width()/2-fontobject.size(ask)[0]/2, main_surface.get_height()/2-75))
+        pygame.display.flip()
+
+        for evt in pygame.event.get():
+            if evt.type == KEYDOWN:
+                if evt.key == K_RETURN:
+                    main_surface.fill((0, 0, 0))
+                    players.append(addPlayer(main_surface))
+                else:
+                    more_players = False
+                
+
+
+
+
     pg.mixer.init()
     laser_beam_sound = pg.mixer.Sound("laser_beam.wav")
 
--- player.py
+++ player.py
@@ -4,3 +4,10 @@ class Player:
         self.name = name
         self.gender = gender
         self.weight = weight
+
+def addPlayer(disp):
+    name = Input_field(disp, "Name", False)
+    gender = Select_field(disp, "Gender", ["Man","Woman"])
+    weight = Input_field(disp, "Weight in kg", True)
+    disp.fill((0, 0, 0))
+    return Player(name,gender,weight)
